6ed02bafb1d34b3315089d0430bcb4290467b3e6
Cemile Aksu <cemileeaksuu@gmail.com>
2026-07-28 12:56:42 +0300

    feat: initialize Spring Boot backend and database connection

--- backend/src/main/java/com/kolaysoft/weeklyprojectstatus/WeeklyProjectStatusApplication.java
+++ backend/src/main/java/com/kolaysoft/weeklyprojectstatus/WeeklyProjectStatusApplication.java
@@ -1,13 +1,13 @@
-package com.cemileaksu.weeklyprojectstatus;
+package com.kolaysoft.weeklyprojectstatus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class WeeklyProjectStatusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(WeeklyProjectStatusApplication.class, args);
 	}
 
 }
